be2096256ccdcef7eb4dffe8d42273fb75c05b68
MicmladenTo <micmladento@gmail.com>
2021-10-29 01:57:52 +0200

    Assertions added to the tests
    - Explicit waiters replaced with cy.wait()
    - Parts of code commented out for future reference (learning purposes), as well as because some parts of the web app have been changed slightly in the meantime.

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,60 +2,134 @@ import loginPage from '../fixtures/loginModule.json';
 import data from '../fixtures/data.json';
 import sidebar from '../fixtures/sidebar.json';
 import navigation from '../fixtures/navigation.json';
-// import login from '../fixtures/loginModule.json'
 
-describe('first cypress block', () => {
+describe('A suite of login tests', () => {
 
-	it('visit vivify scrum', () => {
+	before(() => {
+		cy.intercept('/api/v2/health-check').as('loginRedirect');
 		cy.visit('/', {timeout: 30000});
+
+		// Make sure we have been properly redirected at the beginning of the suite
+		cy.wait('@loginRedirect')
+		.its('response')
+		.then((res) => {
+		expect(res.statusCode).to.eq(200);
+		})
 	});
 
+	beforeEach(() => {
+		// Make sure our input fields are empty before every test
+		cy.get(loginPage.emailInput).clear().should('have.value', '');
+		cy.get(loginPage.passwordInput).clear().should('have.value', '');
+	})
+
 	it('no username login', () => {
-		cy.get(loginPage.passwordInput).clear().type(data.user.password);
+		// Make sure we are at the correct URL before the first test in the suite
+		cy.url().should('eq', 'https://cypress.vivifyscrum-stage.com/login')
+		cy.get(loginPage.passwordInput).type(data.user.password);
 		cy.get(loginPage.loginButton).click();
+		// Assert that the error message pertains to an invalid email message
+		cy.get(loginPage.errorFields).eq(0).should('contain', 'The email field must be a valid email');
 	});
 
 	it('no password login', () => {
-		cy.get(loginPage.emailInput).clear().type(data.user.email);
-		cy.get(loginPage.passwordInput).clear();
+		cy.get(loginPage.emailInput).type(data.user.email);
 		cy.get(loginPage.loginButton).click();
+		// Assert that the error message pertains to an invalid email message
+		cy.get(loginPage.errorFields).eq(1).should('contain', 'The password field is required');
 	});
 
 	it('wrong username login', () => {
-		cy.get(loginPage.emailInput).clear().type(data.wrongUser.wrongEmail);
-		cy.get(loginPage.passwordInput).clear().type(data.user.password);
+		// Intercept the login POST route
+		cy.intercept('POST', '/api/v2/login').as('login');
+		cy.get(loginPage.emailInput).type(data.wrongUser.wrongEmail);
+		cy.get(loginPage.passwordInput).type(data.user.password);
 		cy.get(loginPage.loginButton).click();
+		//Assert that the error message pertains to mismatched login data
+		cy.get(loginPage.loginError).should('contain', 'Oops! Your email/password combination is incorrect');
+
+		// Assert the "unauthorised request" network response
+		cy.wait('@login')
+		.its('response')
+		.then((res) => {
+		expect(res.statusCode).to.eq(401);
+		})
 	});
 
 	it('wrong password login', () => {
-		cy.get(loginPage.emailInput).clear().type(data.user.email);
-		cy.get(loginPage.passwordInput).clear().type(data.wrongUser.wrongPassword);
+		// Intercept the login POST route
+		cy.intercept('POST', '/api/v2/login').as('login');
+		cy.get(loginPage.emailInput).type(data.user.email);
+		cy.get(loginPage.passwordInput).type(data.wrongUser.wrongPassword);
 		cy.get(loginPage.loginButton).click();
+		//Assert that the error message pertains to mismatched login data
+		cy.get(loginPage.loginError).should('contain', 'Oops! Your email/password combination is incorrect');
+
+		// Assert the "unauthorised request" network response
+		cy.wait('@login')
+		.its('response')
+		.then((res) => {
+		expect(res.statusCode).to.eq(401);
+		})
 	});
 
 	it('invalid user login', () => {
-		cy.get(loginPage.emailInput).clear().type(data.invalidUser.invalidUser);
-		cy.get(loginPage.passwordInput).clear().type(data.user.password);
+		cy.get(loginPage.emailInput).type(data.invalidUser.invalidUser);
+		cy.get(loginPage.passwordInput).type(data.user.password);
 		cy.get(loginPage.loginButton).click();
+		// Assert that the error message pertains to an invalid email message
+		cy.get(loginPage.errorFields).eq(0).should('contain', 'The email field must be a valid email');
 	});
 
 	it('invalid password login', () => {
-		cy.get(loginPage.emailInput).clear().type(data.user.email)
-		cy.get(loginPage.passwordInput).clear().type(data.invalidUser.invalidPassword);
+		cy.get(loginPage.emailInput).type(data.user.email)
+		cy.get(loginPage.passwordInput).type(data.invalidUser.invalidPassword);
 		cy.get(loginPage.loginButton).click();
+		// Assert that the error message pertains to an invalid email message
+		cy.get(loginPage.errorFields).eq(1).should('contain', 'The password field must be at least 5 characters');
 	});
 
-	it('valid login', () => {
-		cy.get(loginPage.emailInput).clear().type(data.user.email);
-		cy.get(loginPage.passwordInput).clear().type(data.user.password);
-		cy.get(loginPage.loginButton).click();
-	});
+	it.only('Valid login, then logout', () => {
+		// Intercept the login, logout and my-organization routes
+		cy.intercept('POST', '/api/v2/login').as('login');
+		cy.intercept('POST', '/api/v2/logout').as('logout');
+		cy.intercept('GET', '/api/v2/my-organizations').as('myOrganizations');
 
-	it('log out', () => {
+		cy.get(loginPage.emailInput).type(data.user.email);
+		cy.get(loginPage.passwordInput).type(data.user.password);
+		cy.get(loginPage.loginButton).click();
+
+		// Assert that login attempt has been authorised
+		cy.wait('@login')
+		.its('response')
+		.then((res) => {
+		expect(res.statusCode).to.eq(200);
+		})
+
+		// Assert that we have successfully requested organization list upon logging in
+		cy.wait('@myOrganizations')
+		.its('response')
+		.then((res) => {
+		expect(res.statusCode).to.eq(200);
+		})
+
+		// Make sure that the browser has loaded the correct URL
+		cy.url().should('eq', 'https://cypress.vivifyscrum-stage.com/my-organizations');
+
+		// Go to Account settings section and assert that we are at the right place
 		cy.get(sidebar.account).click();
 		cy.get(sidebar.profileSettings).click();
+		cy.url().should('eq', 'https://cypress.vivifyscrum-stage.com/account/settings');
+
+		// Logout and assert that the request is valid
 		cy.get(navigation.logoutButton).click();
+		cy.wait('@logout')
+		.its('response')
+		.then((res) => {
+		expect(res.statusCode).to.eq(201);
+		});
+
+		// Assert that we are back at the right screen
+		cy.url().should('eq', 'https://cypress.vivifyscrum-stage.com/login');
 	});
-
-
   })
--- cypress/integration/registration.spec.js
+++ cypress/integration/registration.spec.js
@@ -9,20 +9,42 @@ import navigation from '../fixtures/navigation.json';
 
 describe('Registration suite', () => { 
 
-	it('Go to registration page', () => {
+	it.only('Go to registration page', () => {
+		cy.intercept('/api/v2/health-check').as('loginRedirect');
 		cy.visit('/', {timeout: 30000});
+		// Make sure we have been properly redirected on visiting the homepage
+		cy.wait('@loginRedirect')
+		.its('response')
+		.then((res) => {
+		expect(res.statusCode).to.eq(200);
+		})
 		cy.get(loginPage.linktoSignup).click();
+		// Make sure we are at the Pricing page
+		cy.url().should('eq', 'https://cypress-api.vivifyscrum-stage.com/pricing')
 	});
 
-	it('Sign up for a starter account', () => {
+	it.only('Sign up for a starter account', () => {
+		cy.intercept('/api/v2/pricing-plans/1').as('registrationForm');
+
 		cy.get(pricingPage.monthlySwitch).click({force:true});
+		// Assert that we see four packages
+		cy.get(pricingPage.visiblePackages).should('be.visible').and('have.length', 4);
 		cy.get(pricingPage.annualPackages.starterSignup).click({force:true});
+
+		// Wait for the registration form and assert that the status code of the request is correct
+		cy.wait('@registrationForm')
+		.its('response')
+		.then((res) => {
+		expect(res.statusCode).to.eq(200);
+		})
 	});
 
 	it('Attempt registration without email', () => {
 		cy.get(registration.password, { timeout: 10000 }).type(data.user.password);
 		cy.get(registration.numberOfUsers).type(data.numberOfUsers.validNumber);
 		cy.get(registration.submit).click();
+		// Assert that the error message pertains to an invalid email message
+		cy.get(registration.errorFields).eq(0).should('contain', 'The email field must be a valid email');
 	});
 
 	it('Attempt registration without the @ in email', () => {
@@ -30,6 +52,8 @@ describe('Registration suite', () => {
 		cy.get(registration.password).clear().type(data.user.password);
 		cy.get(registration.numberOfUsers).clear().type(data.numberOfUsers.validNumber);
 		cy.get(registration.submit).click();
+		// Assert that the error message pertains to an invalid email message
+		cy.get(registration.errorFields).eq(0).should('contain', 'The email field must be a valid email');
 	});
 
 	it('Attempt registration without domain in the email', () => {
@@ -37,6 +61,8 @@ describe('Registration suite', () => {
 		cy.get(registration.password).clear().type(data.user.password);
 		cy.get(registration.numberOfUsers).clear().type(data.numberOfUsers.validNumber);
 		cy.get(registration.submit).click();
+		// Assert that the error message pertains to an invalid email message
+		cy.get(registration.errorFields).eq(0).should('contain', 'The email field must be a valid email');
 	});
 
 	it('Attempt registration without top-level domain in the email', () => {
@@ -44,6 +70,8 @@ describe('Registration suite', () => {
 		cy.get(registration.password).clear().type(data.user.password);
 		cy.get(registration.numberOfUsers).clear().type(data.numberOfUsers.validNumber);
 		cy.get(registration.submit).click();
+		// Assert that the error message pertains to an invalid email message
+		cy.get(registration.errorFields).eq(0).should('contain', 'The email field must be a valid email');
 	});
 
 	it('Attempt registration with an invalid second-level domain name', () => {
@@ -51,6 +79,8 @@ describe('Registration suite', () => {
 		cy.get(registration.password).clear().type(data.user.password);
 		cy.get(registration.numberOfUsers).clear().type(data.numberOfUsers.validNumber);
 		cy.get(registration.submit).click();
+		// Assert that the error message pertains to an invalid email message
+		cy.get(registration.errorFields).eq(0).should('contain', 'The email field must be a valid email');
 	});
 
 	it('Attempt registration without an email username', () => {
@@ -58,6 +88,8 @@ describe('Registration suite', () => {
 		cy.get(registration.password).clear().type(data.user.password);
 		cy.get(registration.numberOfUsers).clear().type(data.numberOfUsers.validNumber);
 		cy.get(registration.submit).click();
+		// Assert that the error message pertains to an invalid email message
+		cy.get(registration.errorFields).eq(0).should('contain', 'The email field must be a valid email');
 	});
 
 	it('Attempt registration with special characters in email username', () => {
@@ -65,6 +97,8 @@ describe('Registration suite', () => {
 		cy.get(registration.password).clear().type(data.user.password);
 		cy.get(registration.numberOfUsers).clear().type(data.numberOfUsers.validNumber);
 		cy.get(registration.submit).click();
+		// Assert that the error message pertains to an invalid email message
+		cy.get(registration.errorFields).eq(0).should('contain', 'The email field must be a valid email');
 	});
 
 	it('Attempt registration without a password', () => {
@@ -72,6 +106,8 @@ describe('Registration suite', () => {
 		cy.get(registration.password).clear();
 		cy.get(registration.numberOfUsers).clear().type(data.numberOfUsers.validNumber);
 		cy.get(registration.submit).click();
+		// Assert that the error message pertains to the lack of pasword
+		cy.get(registration.errorFields).eq(1).should('contain', 'The password field is required');
 	});
 
 	// it('Attempt registration with an invalid password', () => {
@@ -86,6 +122,8 @@ describe('Registration suite', () => {
 		cy.get(registration.password).clear().type(data.invalidPassword.tooShort);
 		cy.get(registration.numberOfUsers).clear().type(data.numberOfUsers.validNumber);
 		cy.get(registration.submit).click();
+		// Assert that the error message pertains to password length
+		cy.get(registration.errorFields).eq(1).should('contain', 'The password field must be at least 5 characters');
 	});
 
 	it('Attempt registration without a number of users', () => {
@@ -93,6 +131,8 @@ describe('Registration suite', () => {
 		cy.get(registration.password).clear().type(data.user.password);
 		cy.get(registration.numberOfUsers).clear();
 		cy.get(registration.submit).click();
+		// Assert that the error message pertains to the required "number of users" value
+		cy.get(registration.errorFields).eq(2).should('contain', 'The number of users field is required');
 	});
 
 	it('Attempt registration with no users', () => {
@@ -100,6 +140,8 @@ describe('Registration suite', () => {
 		cy.get(registration.password).clear().type(data.user.password);
 		cy.get(registration.numberOfUsers).clear().type(data.numberOfUsers.tooFew);
 		cy.get(registration.submit).click();
+		// Assert that the error message pertains to the inadequate number of users
+		cy.get(registration.errorFields).eq(2).should('contain', 'The number of users must be between 1 and 10');
 	});
 
 	it('Attempt registration with more users than allowed', () => {
@@ -107,31 +149,85 @@ describe('Registration suite', () => {
 		cy.get(registration.password).clear().type(data.user.password);
 		cy.get(registration.numberOfUsers).clear().type(data.numberOfUsers.tooMany);
 		cy.get(registration.submit).click();
+		// Assert that the error message pertains to the inadequate number of users
+		cy.get(registration.errorFields).eq(2).should('contain', 'The number of users must be between 1 and 10');
 	});
 
-	it('Register successfully', () => {
+	it.only('Register successfully', () => {
+		cy.intercept('POST', '/api/v2/register').as('register');
+		cy.intercept('GET', '/api/v2/my-organizations').as('myOrganizations');
 		cy.get(registration.email).clear().type(data.user.email);
 		cy.get(registration.password).clear().type(data.user.password);
 		cy.get(registration.numberOfUsers).clear().type(data.numberOfUsers.validNumber);
 		cy.get(registration.submit).click();
+
+		// Assert that registration POST request was successful
+		cy.wait('@register')
+		.its('response')
+		.then((res) => {
+		expect(res.statusCode).to.eq(200);
+		})
+
+		// Assert that "My organizations" have been requested
+		cy.wait('@myOrganizations')
+		.its('response')
+		.then((res) => {
+		expect(res.statusCode).to.eq(200);
+		})
+
+		// Make sure that the browser has loaded the correct URL
+		cy.url().should('eq', 'https://cypress.vivifyscrum-stage.com/my-organizations');
 	});
 
-	it('Log in again and add account details', () => {
-		// Log out
+	it.only('Add account details and log out', () => {
+		cy.intercept('POST', '/api/v2/logout').as('logout');
+
+		// Go to Account section and assert that we are at the expected page
 		cy.get(sidebar.account).click();
-		cy.get(sidebar.profileSettings).click();
-		cy.get(navigation.logoutButton).click();
-		// Log back in
-		cy.get(loginPage.emailInput).type(data.user.email);
-		cy.get(loginPage.passwordInput).type(data.user.password);
-		cy.get(loginPage.loginButton).click();
-		cy.wait(4000);
-		// Add account details
-		cy.get(registration.accountDetailsModal.firstName).type(data.accountDetails.firstName);
-		cy.get(registration.accountDetailsModal.lastName).type(data.accountDetails.lastName);
-		cy.get(registration.accountDetailsModal.companyName).type(data.accountDetails.companyName);
-		cy.get(registration.accountDetailsModal.organisationName).type(data.accountDetails.organisationName);
+
+		// Add account details and assert that the correct values have been put in the modal
+		cy
+		.get(registration.accountDetailsModal.firstName)
+		.type(data.accountDetails.firstName)
+		.should('have.value', data.accountDetails.firstName);
+		cy
+		.get(registration.accountDetailsModal.lastName)
+		.type(data.accountDetails.lastName)
+		.should('have.value', data.accountDetails.lastName);;
+		cy
+		.get(registration.accountDetailsModal.companyName)
+		.type(data.accountDetails.companyName)
+		.should('have.value', data.accountDetails.companyName);;
+		cy
+		.get(registration.accountDetailsModal.organisationName)
+		.type(data.accountDetails.organisationName)
+		.should('have.value', data.accountDetails.organisationName);;
 		cy.get(registration.accountDetailsModal.submitButton).click();
+
+		// Assert that the modal is no longer visible
+		cy.get(registration.accountDetailsModal).should("not.be.visible");
+
+		// If explicit wait is not used, the same modal reappears during automated testing
 		cy.wait(2000);
+
+		// Go to Account settings section and assert that we are at the right place
+		cy.get(sidebar.profileSettings).click();
+		cy.url().should('eq', 'https://cypress.vivifyscrum-stage.com/account/settings');
+
+		// Logout and assert that the request is valid
+		cy.get(navigation.logoutButton).click();
+		cy.wait('@logout')
+		.its('response')
+		.then((res) => {
+		expect(res.statusCode).to.eq(201);
+		});
+
+		// Assert that we are back at the login screen
+		cy.url().should('eq', 'https://cypress.vivifyscrum-stage.com/login');
+
+		// Log back in
+		// cy.get(loginPage.emailInput).type(data.user.email);
+		// cy.get(loginPage.passwordInput).type(data.user.password);
+		// cy.get(loginPage.loginButton).eq(0).click();
 	});
   })
